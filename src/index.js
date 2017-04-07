1936b6257d717f984519667fe5d4b8f61434f1b7
Jaakko Tulkki <jaakko.tulkki@sainsburys.co.uk>
2017-04-07 16:30:39 +0100

    failing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 import Header from './layout/Header';
 import WriterView from './ui/Writer';
-
-import {
-BrowserRouter as Router,
-Route,
-} from 'react-router-dom'
-
+import { Route }from 'react-router-dom'
 import './index.css';
+import reducers from './reducer';
 
 
 const HomePage = () => {
@@ -17,17 +17,31 @@ const HomePage = () => {
   );
 };
 
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+)
+
 const Routing = () => (
-  <Router>
+  <ConnectedRouter>
     <div>
       <Header />
       <Route exact path="/" component={HomePage}/>
     </div>
-  </Router>
+  </ConnectedRouter>
 );
 
+// Now you can dispatch navigation actions from anywhere!
+// store.dispatch(push('/foo'))
 
 ReactDOM.render(
-  <Routing />,
+  <Provider store={store}>
+    <Routing />
+  </Provider>,
   document.getElementById('root')
 );
